4d3dd36f0ef2f25c55fba949d4966fc4f66019e5
Lucas Saliés Brum <sistematico@gmail.com>
2024-07-07 15:21:35 -0400

    Updated engine.ts(test)

--- backend/src/engine.ts
+++ backend/src/engine.ts
@@ -4,36 +4,37 @@ import * as schema from '@/drizzle/schema'
 import { Glob } from 'bun'
 import { basename, parse } from 'node:path'
 
-const genre = Bun.argv[2] || 'geral'
-const songsPath = Bun.env.SONGS_PATH
-let song: string | null
+// const genre = Bun.argv[2] || 'geral'
+// const songsPath = Bun.env.SONGS_PATH
+// let song: string | null
 
-async function getRandomSong(genre = 'geral') {
+async function getRandomSong() {
   const [song] = await db.select()
     .from(schema.songs)
-    .where(eq(schema.songs.genre, genre))
+    // .where(eq(schema.songs.genre, genre))
     .orderBy(sql`RANDOM()`)
     .limit(1)
 
-  if (!song) return null
   return song.path
 }
 
-async function getRandomFile() {
-  const glob = new Glob("**/*.mp3")
-  const files = []
+// async function getRandomFile() {
+//   const glob = new Glob("**/*.mp3")
+//   const files = []
 
-  for await (const file of glob.scan(songsPath)) {
-    const filename = parse(basename(file)).name
-    const title = filename.split('-').pop()
-    const artist = filename.split('-')[0]
-    files.push({ artist: artist.trim(), title: title?.trim() || '', path: `${songsPath}/${file}` })
-  }
+//   for await (const file of glob.scan(songsPath)) {
+//     const filename = parse(basename(file)).name
+//     const title = filename.split('-').pop()
+//     const artist = filename.split('-')[0]
+//     files.push({ artist: artist.trim(), title: title?.trim() || '', path: `${songsPath}/${file}` })
+//   }
 
-  return files[Math.floor(Math.random() * files.length)].path
-}
+//   return files[Math.floor(Math.random() * files.length)].path
+// }
 
-song = await getRandomSong(genre)
-if (!song) song = await getRandomFile()
+// song = await getRandomSong(genre)
+// if (!song) song = await getRandomFile()
 
-Bun.write(Bun.stdout, song)
+// const song = await getRandomSong()
+
+Bun.write(Bun.stdout, await getRandomSong())
--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ansible-playbook --connection=local -e "ansible_port=2200" /var/www/somdomato/ansible/playbook.yml -i localhost,
+if [ -f /etc/arch-release ]; then
+  ansible-playbook -e "ansible_port=2200 ansible_python_interpreter=/usr/bin/python3" /home/lucas/code/somdomato/somdomato/ansible/playbook.yml -i ananke.paxa.dev,
+else
+  ansible-playbook --connection=local -e "ansible_port=2200" /var/www/somdomato/ansible/playbook.yml -i localhost,
+fi
