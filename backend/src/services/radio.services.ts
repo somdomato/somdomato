feea8744c7ea836936649caf4ebfc37a3fa4a3ef
Lucas Saliés Brum <sistematico@gmail.com>
2024-07-13 07:04:22 -0400

    Update

--- backend/src/services/radio.services.ts
+++ backend/src/services/radio.services.ts
@@ -1,9 +1,11 @@
 import { Glob } from 'bun'
 
+const SONGS = Bun.env.SONGS_PATH as string
+
 export async function getRandomFile(genre: string): Promise<string> {
   const files = []
-  const songsPath = `${Bun.env.SONGS_PATH}${genre}`  
-  const glob = new Glob(`${songsPath}**/*.mp3`)
+  const songsPath = `${SONGS}/${genre}`  
+  const glob = new Glob(`${songsPath}/**/*.mp3`)
   
   for await (const file of glob.scan(songsPath)) {
     files.push(`${file}`)
