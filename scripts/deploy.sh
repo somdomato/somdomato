c996c117b872a0068cc86bb2f4b8bcd3d3b2996b
Lucas Saliés Brum <sistematico@gmail.com>
2024-07-19 21:05:53 -0400

    Update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
 PATH=$PATH:/home/nginx/.bun/bin
+RESET=1
 
 cd backend
+
+if [ $RESET == 1 ]; then
+  rm -fr sqlite.db src/drizzle/migrations/
+  sudo /usr/bin/systemctl stop somdomato-hono.service
+fi
+
 [ ! -f .env ] && cp .env.prod .env
-#rm -fr sqlite.db src/drizzle/migrations/
+
 bun install
 bun run generate
 bun run migrate
 bun run seed
 
 cd ../frontend
+
 [ ! -f .env ] && cp .env.prod .env
-#rm -fr dist/
+
 bun install
 bun run build
 
-#sudo /usr/bin/systemctl restart somdomato-hono.service
+if [ $RESET == 1 ]; then
+  sudo /usr/bin/systemctl start somdomato-hono.service
+else
+  sudo /usr/bin/systemctl restart somdomato-hono.service
+fi
