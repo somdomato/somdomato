92128fd6378b174ba610aa9dad40950e9c85dec7
Lucas Saliés Brum <sistematico@gmail.com>
2024-07-21 20:31:15 -0400

    Update

--- frontend/src/components/player.vue
+++ frontend/src/components/player.vue
@@ -1,34 +1,23 @@
 <script setup lang="ts">
 import { ref, watch, onMounted } from 'vue'
 import { useSongStore } from '@/stores/song'
-import type { Song } from '@/types'
+// import type { Song } from '@/types'
 
 const song = useSongStore()
 const streamUrl = 'https://radio.somdomato.com'
-const src = ref(`${streamUrl}/radio.ogg`)
+const src = ref(`${streamUrl}/radio.mp3`)
+const oggSrc = ref(`${streamUrl}/radio.ogg`)
 
-async function reloadPlayerGenre(player: HTMLAudioElement, source: HTMLSourceElement, genre: string) {
-  player.pause()
-  source.src = `${streamUrl}/${genre}?ts=${+new Date()}`
-  player.load()
-}
-
-async function reloadPlayer(player: HTMLAudioElement, source: HTMLSourceElement) {
-  player.pause()
-  source.src = `${streamUrl}/radio.ogg?ts=${+new Date()}`
-  player.load()
-}
-
-async function multiSongName() {
-  const { icestats: { source } } = await (await fetch(`${streamUrl}/json`)).json()
-  const result = source.find((item: Song) => item.genre.toLocaleLowerCase() === song.genre)
-  song.title = !result ? song.setTitle('Rádio Som do Mato') : result.title.normalize("NFD")  
-}
+// async function multiSongName() {
+//   const { icestats: { source } } = await (await fetch(`${streamUrl}/json`)).json()
+//   const result = source.find((item: Song) => item.genre.toLocaleLowerCase() === song.genre)
+//   song.title = !result ? song.setTitle('Rádio Som do Mato') : result.title.normalize("NFD")  
+// }
 
 async function songName() {
   const { icestats: { source: { artist, title } } } = await (await fetch(`${streamUrl}/json`)).json()
-  const normalizedTitle = artist.normalize('NFD') + ' - ' + title.normalize('NFD')  
-  if (!title || title === '') {
+  const normalizedTitle = `${artist.normalize('NFD')} - ${title.normalize('NFD')}`  
+  if (!title || title === 'undefined' || title === '') {
     song.setTitle('Rádio Som do Mato')
   } else {
     song.setTitle(normalizedTitle)
@@ -37,16 +26,19 @@ async function songName() {
 
 onMounted(async () => {
   const player = document.querySelector('audio') as HTMLAudioElement
-  const source = document.querySelector('source') as HTMLSourceElement
   const restart = document.querySelector('.plyr__restart') as HTMLElement
+  const sourceOgg = player.querySelector("source[type='audio/ogg']") as HTMLSourceElement
+  const sourceMpeg = player.querySelector("source[type='audio/mp3']") as HTMLSourceElement
 
   restart.addEventListener('click', async () => {
-    reloadPlayer(player, source)
+    sourceMpeg.src = src.value
+    sourceOgg.src = oggSrc.value
+    player.load()
     player.play()
     await songName()
   })
   
-  reloadPlayer(player, source)
+  // reloadPlayer(player, source)
   await songName()
   
   setInterval(async () => {
@@ -57,9 +49,9 @@ onMounted(async () => {
 watch(
   () => song.genre,
   () => {
-    reloadPlayer(document.querySelector('audio') as HTMLAudioElement, document.querySelector('source') as HTMLSourceElement);
+    // reloadPlayer(document.querySelector('audio') as HTMLAudioElement, document.querySelector('source') as HTMLSourceElement);
     
-    (document.querySelector('audio') as HTMLAudioElement).addEventListener("canplay", (event) => {
+    (document.querySelector('audio') as HTMLAudioElement).addEventListener("canplay", () => {
       (document.querySelector('audio') as HTMLAudioElement).play();
     });
   }
@@ -69,6 +61,7 @@ watch(
   <div class="d-md-flex align-items-center justify-content-between">
     <vue-plyr ref="plyr">
       <audio controls crossorigin="true" playsinline data-plyr-config='{ "tooltips": { "controls": "false", "seek": "false" } }'>
+        <source :src="oggSrc" type="audio/ogg" />
         <source :src type="audio/mp3" />
       </audio>
     </vue-plyr>
