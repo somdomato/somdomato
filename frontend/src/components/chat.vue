38ed88922e2dfb1334b530bed7ca7183a53af38e
Lucas Saliés Brum <sistematico@gmail.com>
2024-07-25 08:02:01 -0400

    Update

--- frontend/src/components/chat.vue
+++ frontend/src/components/chat.vue
@@ -1,11 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const open = ref(false)
+</script>
 <template>
   <div class="card">
-    <div class="card-header">
-      <h5 class="card-title">Bate-Papo</h5>
+    <div class="card-header d-flex align-items-center justify-content-between">
+      <h5 class="card-title mb-0">Bate-Papo</h5>
+      <button class="btn btn-sm btn-danger" @click="open = !open" v-if="open">Fechar</button>
     </div>
-    <div class="card-body p-0">
-      <div class="chat-container"> 
+    <div class="d-flex align-items-center justify-content-center card-body p-0" style="min-height: 500px;">
+      <div class="chat-container" v-if="open"> 
         <iframe class="chat-iframe" src="https://chat.somdomato.com" />
+      </div>
+      <div class="d-flex flex-column align-items-center justify-content-center" v-else>
+        <img class="img-fluid" src="/images/logo-v2.svg" alt="Rádio Som do Mato" />
+        <button class="btn btn-lg btn-danger mt-2" @click="open = !open">Entrar no Bate-Papo</button>
       </div>
     </div>
   </div>
--- frontend/src/components/pulse.vue
+++ frontend/src/components/pulse.vue
@@ -1,6 +1,41 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const online = ref(false)
+const url = 'https://radio.somdomato.com' 
+
+function check() {
+  fetch(url)
+    .then((response) => {
+      console.log("response.type =", response.type);
+      console.log("response.url =", response.url);
+      console.log("response.status =", response.status);
+      console.log("response.ok =", response.ok);
+      console.log("response.statusText =", response.statusText);
+      console.log("response.headers =", response.headers);
+      if (!response.ok) {
+        throw new Error(`HTTP error, status = ${response.status}`);
+      }
+
+      if (response.ok) {
+        online.value = true
+      } else {
+        online.value = false
+      }
+    })
+    .catch((error) => {
+      console.log(`Error: ${error.message}`)
+      online.value = false
+    })
+}
+
+onMounted(() => {
+  setInterval(check, 10000)
+})
+</script>
 <template>
   <div class="pulse-container">
-    <div class="pulse"></div>
+    <div :class="{ 'pulse': online, 'pulse-red': !online }"></div>
   </div>
 </template>
 <style lang="scss">
@@ -20,19 +55,28 @@
   display: block;
   height: 7px;
   width: 7px;
-  background-color: #FFC700;
+  background-color: #1aff00;
   border-radius: 50%;
-  // top: 8px;
-  // left: -10px;
   animation: pulse-animation 1.3s infinite;
 }
 
 @keyframes pulse-animation {
-  0% {
-    box-shadow: 0 0 0 0px rgba(249, 179, 0, 0.5);
-  }
-  100% {
-    box-shadow: 0 0 0 10px rgba(249, 179, 0, 0);
-  }
+  0% { box-shadow: 0 0 0 0px rgba(37, 249, 0, 0.5); }
+  100% { box-shadow: 0 0 0 10px rgba(4, 249, 0, 0); }
+}
+
+.pulse-red {
+  position: relative;
+  display: block;
+  height: 7px;
+  width: 7px;
+  background-color: #c91919;
+  border-radius: 50%;
+  animation: pulse-red-animation 1.3s infinite;
+}
+
+@keyframes pulse-red-animation {
+  0% { box-shadow: 0 0 0 0px rgba(249, 0, 0, 0.5); }
+  100% { box-shadow: 0 0 0 10px rgba(249, 0, 0, 0); }
 }
 </style>
